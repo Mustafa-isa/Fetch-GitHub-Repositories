a16ab79fe938d695dc61db5c7034b4aab768330b
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2023-02-08 18:07:46 +0200

    end
    end

--- main.js
+++ main.js
@@ -17,8 +17,22 @@ fetch(`https://api.github.com/users/${input.value}/repos`)
 .then(rsponse =>{
     return rsponse.json()
 })
-.then(data =>{
-    console.log(data)
+
+.then(data =>{   
+dataShaow.innerHTML =''
+    for(let  i=0 ; i <data.length ; i++){
+        console.log(data[i])
+        let her =`https://api.github.com/users/${input.value}/${data[i].name}`
+       dataShaow.innerHTML += ` <div class="main">
+       <span>${data[i].name}</span>
+       <div class="info">
+           <span class="span1"> V:${data[i].visibility
+} </span>
+           <span class="span2"><a href= ${her}>visit</a></span>
+       </div>
+   </div>    `
+    }
+   
 })
 
 
